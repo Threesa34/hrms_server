be51fbcded53ea779f63dadb657017f3c02190f9
Threesa34 <threesainfoway00@gmail.com>
2021-02-01 14:23:42 +0530

    employee details 75% completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hrms_server
+
+# packeges install:     npm install body-parser bower cookie-parser cors crypto express html-pdf jsonwebtoken moment multer mysql mysqldump node-schedule nodemailer phantomjs phantomjs-prebuilt read-excel-file socket.io xlsx --save
---
+++ server.js
@@ -0,0 +1,63 @@
+
+ var dbcreation = require('./lib/config/dbCreation');	
+var express = require('express'),
+	path = require('path'),
+	bodypareser = require('body-parser'),
+	fs = require('fs'),
+	env = require('./lib/config/env'),
+	cryptconf = require('./lib/config/crypt.config');
+	var crypto = require('crypto');
+	var routes = require('./lib/routes');
+	var dbBackup = require('./lib/config/dbBackup');
+	var app = express();
+
+	let http = require('http').Server(app);
+
+app.use(bodypareser.urlencoded({limit:'100mb',extended:true}));
+app.use(bodypareser.json({limit:'100mb'}));
+	
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "OPTIONS, POST, GET, PUT, DELETE");
+	res.header("Access-Control-Allow-Headers", "*");
+	res.header('Access-Control-Allow-Credentials', true);
+	if ('OPTIONS' == req.method) {
+		return res.sendStatus(200);
+	} else {
+		next();
+	}
+  });
+
+app.use(express.static(path.join(__dirname,'app')));
+
+routes.configure(app);
+
+	dbcreation.createDB();
+	dbcreation.CreateTables();
+
+dbBackup.GenerateBackup();
+/* var key_fields = Object.keys(env)
+ if(key_fields.length > 0)
+ {
+ key_fields.map(function(val){
+	 try{
+		console.log(val+" = "+cryptconf.decrypt(env[String(val)]));
+	 }
+	 catch(exp)
+	 {
+		 
+	 }
+	
+}); 
+} */
+
+var server = app.listen(parseInt(cryptconf.decrypt(env.port)),function(){
+	 console.log('server start', cryptconf.decrypt(env.port));
+})	
+
+ let io = require('socket.io')(server);
+	require('./lib/config/socket.Ctrl')(io);
+
+	server.timeout = 600000;
+
